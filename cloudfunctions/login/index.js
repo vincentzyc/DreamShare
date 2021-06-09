8347aad84900209e9a84d474bf3a260726120ab4
vincentzyc <vincentzyc@qq.com>
2021-06-09 23:16:19 +0800

    init: init

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -18,7 +18,7 @@ cloud.init({
 exports.main = async (event, context) => {
   console.log(event)
   console.log(context)
-  const db = wx.cloud.database()
+  const db = cloud.database()
 
   // 可执行其他自定义逻辑
   // console.log 的内容可以在云开发云函数调用日志查看
