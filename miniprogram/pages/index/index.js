1058f5c01f318d3ff99d2e26290feeaa85cde61f
vincentzyc <vincentzyc@qq.com>
2021-06-09 09:56:35 +0800

    init: init

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,10 +5,11 @@ Page({
   data: {
     avatarUrl: './user-unlogin.png',
     userInfo: {},
+    openid: '',
     hasUserInfo: false,
     requestResult: '',
-    canIUseOpenData:true
-    // canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') // 如需尝试获取用户信息可改为false
+    // canIUseOpenData:false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') // 如需尝试获取用户信息可改为false
   },
 
   onLoad: function () {
@@ -19,27 +20,36 @@ Page({
       return
     }
   },
-  onGetUserInfo: function(e) { 
-    if (e.detail.userInfo) { 
-      console.log(e);
-      this.setData({ 
-        avatarUrl: e.detail.userInfo.avatarUrl, 
-        userInfo: e.detail.userInfo, 
-        hasUserInfo: true, 
-      }) 
-    } 
-  }, 
+  onLogin: function () {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        app.globalData.openid = res.result.openid
+        this.setData({
+          openid: res.result.openid
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '获取 openid 失败，请检查是否有部署 login 云函数',
+        })
+        console.log('[云函数] [login] 获取 openid 失败，请检查是否有部署云函数，错误信息：', err)
+      }
+    })
+  },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res);
         this.setData({
           avatarUrl: res.userInfo.avatarUrl,
           userInfo: res.userInfo,
           hasUserInfo: true,
         })
+        this.onLogin()
       }
     })
   }
