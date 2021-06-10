1b3c7f69aa07bcc78ee2dc1ee8b372d243dcb5bf
vincentzyc <vincentzyc@qq.com>
2021-06-10 10:10:47 +0800

    init: init

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -8,6 +8,32 @@ cloud.init({
   // API 调用都保持和云函数当前所在环境一致
   env: cloud.DYNAMIC_CURRENT_ENV
 })
+/**
+ * 时间格式化
+ * @param {格式化时间格式,yyyy-mm-dd HH:MM:SS} fmt 
+ * @param {需要格式化的时间对象,默认new Date()} date 
+ */
+function formatDate(fmt, date) {
+  fmt = fmt || 'yyyy-mm-dd HH:MM:SS'
+  date = date || new Date()
+  let ret;
+  const opt = {
+    "y+": date.getFullYear().toString(),        // 年
+    "m+": (date.getMonth() + 1).toString(),     // 月
+    "d+": date.getDate().toString(),            // 日
+    "H+": date.getHours().toString(),           // 时
+    "M+": date.getMinutes().toString(),         // 分
+    "S+": date.getSeconds().toString()          // 秒
+    // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  };
+  for (let k in opt) {
+    ret = new RegExp("(" + k + ")").exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+    };
+  };
+  return fmt;
+}
 
 /**
  * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
@@ -27,29 +53,35 @@ exports.main = async (event, context) => {
 
   const openid = wxContext.OPENID
   //获取数据库中有没有当前用户的信息
-  var res = await db.collection("user").where({
+  const res = await db.collection("user").where({
     _openid: openid
-  }).count()
+  }).get()
 
-  if (res.total > 0) {
+  let userType = 1
+
+  if (res.data.length > 0) {
+    userType = res.data[0].user_type
     await db.collection('user').where({
       _openid: openid
     }).update({
       data: {
-        last_login_time: Date.now().toString()
+        last_login_time: formatDate()
       }
     })
   } else {
+    userType = 1
     await db.collection('user').add({
       data: {
+        _openid: openid,
         user_type: 1,
-        register_time: Date.now().toString()
+        register_time: formatDate()
       }
     })
   }
 
   return {
     event,
+    userType,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
     unionid: wxContext.UNIONID,
