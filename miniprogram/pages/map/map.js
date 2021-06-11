9070e556ea2ca7c7bd9d855aa750448500186ad7
vincentzyc <vincentzyc@qq.com>
2021-06-11 09:51:30 +0800

    init: init

---
+++ miniprogram/pages/map/map.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    latitude: 22.834591,
+    longitude: 113.351277,
+    markers: [{
+      latitude: 22.834591,
+      longitude: 113.351277,
+      name: '君享英语'
+    }],
+    covers: [{
+      latitude: 22.834591,
+      longitude: 113.351277,
+      iconPath: '/images/location.png'
+    }, {
+      latitude: 22.834591,
+      longitude: 113.351277,
+      iconPath: '/images/location.png'
+    }]
+  }
+})
